2d6ac7fa356124081541cd4460ece909fd760674
joshSummerhays <russiansummer@gmail.com>
2017-09-10 22:46:06 -0600

    change text, links, add hover

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,13 +6,6 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit, AfterViewInit {
 
-  // panels;
-  isOpen1: boolean = false;
-  isOpen2: boolean = false;
-  isOpen3: boolean = false;
-  isOpen4: boolean = false;
-  isOpen5: boolean = false;
-
   constructor() { }
 
   ngOnInit() {
@@ -22,15 +15,6 @@ export class LandingComponent implements OnInit, AfterViewInit {
     // this.panels = document.querySelectorAll('.panel');
     // this.panels.forEach(panel => panel.addEventListener('click', this.toggleOpen));
     // this.panels.forEach(panel => panel.addEventListener('transitionend', this.toggleActive));
-  }
-
-
-  toggleOpen(item) {
-    if(item === '1') this.isOpen1 = !this.isOpen1;
-    if(item === '2') this.isOpen2 = !this.isOpen2;
-    if(item === '3') this.isOpen3 = !this.isOpen3;
-    if(item === '4') this.isOpen4 = !this.isOpen4;
-    if(item === '5') this.isOpen5 = !this.isOpen5;
   }
 
   toggleActive(e) {
